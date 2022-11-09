da27431bc57004478d044dd7d2011d1f6c9b11f9
Jander Gomez <jandergb.30@gmail.com>
2022-11-09 16:15:15 -0500

    feat: add connection Pool

--- src/database/db.js
+++ src/database/db.js
@@ -11,7 +11,13 @@ const {
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: DB_DIALECT
+  dialect: DB_DIALECT,
+  pool: {
+    max: 500,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 module.exports = {
