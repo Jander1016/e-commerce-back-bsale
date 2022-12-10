dc4ca2682d0f62c93427d29ba5530b212b60c50a
Jander Gomez <jandergb.30@gmail.com>
2022-12-10 08:42:22 -0500

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const apicache = require('apicache')
+const cors = require("cors");
 
 const v1RouterProduct = require('./v1/routes/productRoutes')
 const v1RouterCategory = require('./v1/routes/categoryRoutes')
 
+server.use(cors())
 const server = express();
 const cache =apicache.middleware
 
